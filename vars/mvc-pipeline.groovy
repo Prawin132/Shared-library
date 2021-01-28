e7c9503e5c591c7969ef82620249849e648f1bb6
Prawin132 <56030257+Prawin132@users.noreply.github.com>
2021-01-28 11:30:33 +0530

    Update mvc-pipeline.groovy

--- vars/mvc-pipeline.groovy
+++ vars/mvc-pipeline.groovy
@@ -1,11 +1,33 @@
+#!/usr/bin/env groovy
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    new Sample(script:this).code()
+    new Sample(script:this).build()
+    new Sample(script:this).test()
+    echo config.reponame
+
 pipeline {
     agent any
     
     tools{
         maven 'maven-3.6.0'
     }
+    
+    environment {
+        REPONAME = "${config.reponame}"
+    }
 
     stages{
+        stage('code'){
+            steps{
+                cleanWs()
+                checkout scm
+            }
+        }
        
         
         stage('build'){
@@ -22,4 +44,5 @@ pipeline {
             }
         }
     }
+}
 }
